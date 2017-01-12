a7a400fe3d90ed2c50256bd6b3f73ab27c702b52
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-12 22:56:36 +0100

    update the comments component

--- src/comment.js
+++ src/comment.js
@@ -3,13 +3,9 @@
  * @authors https://github.com/glayzzle/php-reflection/graphs/contributors
  * @url http://glayzzle.com
  */
-var tunic = require('tunic');
 var util = require('util');
-var parser = require('./comment/parser');
-var grammar = require('tunic/dist/grammars/javascript');
-grammar.namedTags = [
-  'return', 'var', 'property', 'throws', 'param'
-];
+var DocParser = require('doc-parser');
+var reader = new DocParser();
 
 /**
  * Initialize a comment declaration
@@ -21,36 +17,41 @@ grammar.namedTags = [
  */
 var comment = function(node, ast) {
   this.type = node.type;
-/*  ast = tunic.parse(ast[1], grammar);
-  this.summary = ast.blocks[0].description;
+  var doc = reader.parse(ast.lines);
+  this.summary = doc.summary;
+  this.tags = {};
   this.annotations = [];
-  this.tags = [];
-  ast = ast.blocks[0].tags;
-  for (var i = 0; i < ast.length; i++) {
-    this.parseBlock(node, ast[i]);
+  for(var i = 0; i < doc.body.length; i++) {
+    var child = doc.body[i];
+    if (child.kind === 'annotation') {
+      this.annotations.push(child);
+    } else {
+      if (!this.body.hasOwnProperty(child.kind)) {
+        this.body[child.kind] = [];
+      }
+      this.body[child.kind].push(child);
+    }
   }
-  */
 };
 
 /**
  * Gets a list of tags matching to the specified name
  * @param {String} name
- * @return {tag[]}
+ * @return {Object[]}
  */
 comment.prototype.getTags = function(name) {
   var result = [];
-  this.tags.forEach(function(item) {
-    if (item.name === name) {
-      result.push(item);
-    }
-  });
+  name = name.toLowerCase();
+  if (this.body.hasOwnProperty(name)) {
+    result = this.body[name];
+  }
   return result;
 };
 
 /**
  * Gets the first tag that matches to the specified name
  * @param {String} name
- * @return {tag|null}
+ * @return {Object|null}
  */
 comment.prototype.getTag = function(name) {
   var items = this.getTags(name);
@@ -64,7 +65,7 @@ comment.prototype.getTag = function(name) {
 /**
  * Gets a list of annotations matching to the specified name
  * @param {String} name
- * @return {annotation[]}
+ * @return {Object[]}
  */
 comment.prototype.getAnnotations = function(name) {
   var result = [];
@@ -79,7 +80,7 @@ comment.prototype.getAnnotations = function(name) {
 /**
  * Gets the first annotation that matches to the specified name
  * @param {String} name
- * @return {annotation|tag|null}
+ * @return {Object}
  */
 comment.prototype.getAnnotation = function(name) {
   var items = this.getAnnotations(name);
@@ -93,236 +94,30 @@ comment.prototype.getAnnotation = function(name) {
 
 
 /**
- * @protected Helper for exporting this object
+ * Gets the attribute of the specified annotation
+ *
+ * ```js
+ * /**
+ *  * @foo(bar => 'baz')
+ *  * @return void
+ *  *\
+ * var doc = new comment(docNode);
+ * doc.getArgument('return', 'what');
+ * doc.getArgument('foo', 0, 'bar');
+ * ```
+ *
+ *
+ * @param {String} tag
+ * @param {Number} offset
+ * @param {String} name
+ * @return {Object}
  */
-comment.prototype.export = function() {
-  return this;
-};
+comment.prototype.getArgument = function(tag, offset, name) {
+  var what = this.getAnnotation(tag);
 
-
-/**
- * @protected Helper for exporting this object
- */
-comment.prototype.parseBlock = function(node, block) {
-  var desc = block.description;
-  if (block.name) {
-    desc = block.name + ' ' + desc;
-  }
-  if (desc[0] === '(') {
-    // @method(...args...)
-    this.annotations.push(
-      new annotation(
-        block.tag,
-        desc
-      )
-    );
-  } else {
-    // classic annotation
-    this.tags.push(new tag(block.tag, desc));
-  }
-};
-
-
-/**
- * property, variable, constant
- * @constructor variable
- * @extends comment
- * @property {type} type
- */
-var variable = function() {
-  this.type = null;
-  comment.apply(this, arguments);
-};
-util.inherits(variable, comment);
-
-/**
- * @protected Helper for exporting this object
- */
-variable.prototype.parseBlock = function(node, block) {
-  if (!this.type && (block.tag === 'var' || block.tag === 'property')) {
-    this.type = new type(
-      node.getNamespace().resolveClassName(block.name),
-      block.description
-    );
-  } else {
-    comment.prototype.parseBlock.apply(this, arguments);
-  }
-};
-
-/**
- * method / function
- * @constructor method
- * @extends variable
- * @property {throws[]} throws
- * @property {param[]} params
- */
-var method = function() {
-  this.params = [];
-  this.throws = [];
-  variable.apply(this, arguments);
-};
-util.inherits(method, variable);
-
-/**
- * @protected Helper for exporting this object
- */
-method.prototype.parseBlock = function(node, block) {
-  if (block.tag === 'return') {
-    this.type = new type(
-      node.getNamespace().resolveClassName(block.name),
-      block.description
-    );
-  } else if (block.tag === 'param') {
-    this.params.push(
-      new param(
-        node.getNamespace().resolveClassName(block.name),
-        block.description
-      )
-    );
-  } else if (block.tag === 'throws') {
-    this.throws.push(
-      new throws(
-        node.getNamespace().resolveClassName(block.name),
-        block.description
-      )
-    );
-  } else {
-    variable.prototype.parseBlock.apply(this, arguments);
-  }
-};
-
-
-/**
- * creates a new comment node
- * @return {comment}
- */
-comment.create = function(node, ast) {
-  if (
-    node.type === 'method' ||
-    node.type === 'function'
-  ) {
-    return new method(node, ast);
-  } else if (
-    node.type === 'property' ||
-    node.type === 'constant' ||
-    node.type === 'variable'
-  ) {
-    return new variable(node, ast);
-  } else {
-    return new comment(node, ast);
-  }
-};
-
-/**
- * @constructor param
- * @property {String} type
- * @property {String} name
- * @property {String} description
- */
-var param = function(type, description) {
-  this.type = type;
-  this.description = description.trim();
-  if (this.description[0] === '$') {
-    // contains the var name
-    for (var i = 0; i < this.description.length; i++) {
-      var c = this.description[i];
-      if (c === ' ' || c === '\t' || c === '\r' || c === '\n') {
-        break;
-      }
-    }
-    this.name = this.description.substring(0, i - 1);
-    this.description = this.description.substring(i);
-  }
-};
-param.prototype.export = function() {
-  return this;
-};
-
-/**
- * @constructor throws
- * @property {String} type
- * @property {String} description
- */
-var throws = function(type, description) {
-  this.type = type;
-  this.description = description;
-}
-throws.prototype.export = function() {
-  return this;
-};
-
-/**
- * @constructor type
- * @property {String} type
- * @property {String} description
- */
-var type = function(name, description) {
-  this.name = name;
-  this.description = description;
-};
-type.prototype.export = function() {
-  return this;
-};
-
-/**
- * @private
- * @constructor tag
- * @property {String} name
- * @property {String} description
- */
-var tag = function(name, description) {
-  this.name = name;
-  this.description = description;
-};
-tag.prototype.export = function() {
-  return this;
-};
-
-/**
- * Parses an argument
- */
-tag.prototype.getArgument = function(offset) {
-  if (!Number.isInteger(offset) || !this.description) {
+  if (what === null) {
     return null;
   }
-  var args = this.description.split(/s+/g);
-  if (offset > -1 && offset < args.length) {
-    return args[offset];
-  }
-  return null;
-};
-
-/**
- * @private
- * @constructor annotation
- * @property {String} name
- * @property {Array} arguments
- * @property {String} description
- */
-var annotation = function(name, args) {
-  this.name = name;
-  this.arguments = [];
-  var methodEnd = args.lastIndexOf(')');
-  if (methodEnd > -1) {
-    this.description = args.substring(methodEnd + 1);
-    var doc = new parser(args.substring(1, methodEnd));
-    this.arguments = doc.ast;
-  }
-};
-/**
- * @private helper for export
- */
-annotation.prototype.export = function() {
-  return this;
-};
-
-/**
- * Gets a named argument (or find it by position)
- * @param {Number} offset The fallback offset when the name is not found
- * @param {String|String[]} name The name of the parameter (or a list of names, aliases)
- * @return {Object|null}
- */
-annotation.prototype.getArgument = function(offset, name) {
 
   // search the node
   if (typeof name === 'undefined' && typeof offset !== 'number') {
@@ -330,17 +125,30 @@ annotation.prototype.getArgument = function(offset, name) {
     offset = null;
   }
   var node = null;
+  var search = [];
+  if (what.kind === 'annotation') {
+    search = what.arguments;
+  } else {
+    for(var k in what) {
+      search.push({
+        kind: 'key',
+        name: k,
+        value: what[k]
+      });
+    }
+  }
 
   // scan by name
   if (name) {
-    if (!Array.isArray(name))
+    if (!Array.isArray(name)) {
       name = [name];
-    for (var i = 0; i < this.arguments.length; i++) {
+    }
+    for (var i = 0; i < search.length; i++) {
       if (
-        this.arguments[i][0] === 'key' &&
-        name.indexOf(this.arguments[i][1]) > -1
+        search.kind === 'key' &&
+        search.name === name
       ) {
-        node = this.arguments[i][2];
+        node = search.value;
         break;
       }
     }
@@ -349,45 +157,24 @@ annotation.prototype.getArgument = function(offset, name) {
   // fallback
   if (node === null && offset != null && offset > -1) {
     // retrieve from offset
-    if (offset < this.arguments.length) {
-      node = this.arguments[offset];
+    if (offset < search.length) {
+      node = search[offset];
       // handle when the parameter is named
-      if (Array.isArray(node) && node[0] === 'key') {
-        if (!name) {
-          node = node[2];
-        } else {
-          // name does not matches with the offset (so ignore it)
-          node = null;
-        }
+      if (node && node.kind && node.kind === 'key') {
+        node = node.value;
       }
     }
   }
 
-  // resolving the node type
-  if (node === null) {
-    return null; // node not found
-  }
-  if (node[0] === 'json') {
-    return node[1];
-  } else if (node[0] === 'array') {
-    return node[1];
-  } else if (node[0] === 'null') {
-    return null;
-  } else if (node[0] === 'text') {
-    node = node[1].substring(1, node[1].length - 1);
-    try {
-      return JSON.parse('"' + node + '"');
-    } catch (e) {
-      return node;
-    }
-  } else if (node[0] === 'number') {
-    return Number.parseInt(node[1]);
-  } else if (node[0] === 'boolean') {
-    return node[1];
-  } else {
-    // unresolved type
-    return node;
-  }
+  return node;
+};
+
+
+/**
+ * @protected Helper for exporting this object
+ */
+comment.prototype.export = function() {
+  return this;
 };
 
 // exports the comment API
