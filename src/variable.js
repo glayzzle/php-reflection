22709c2cf1ba7c910a1e2f14a2832fd7cb526300
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-15 12:29:16 +0100

    add new & parameter parsing for variables

--- src/variable.js
+++ src/variable.js
@@ -6,6 +6,7 @@
 
 
 var node = require('./node');
+var reference = require('./reference');
 
 /**
  * **Extends from {@link NODE.md|:link: node}**
@@ -15,6 +16,7 @@ var node = require('./node');
  * @public
  * @constructor variable
  * @property {String} name
+ * @property {String|Class|Null} type
  */
 var variable = node.extends('variable');
 
@@ -23,8 +25,47 @@ var variable = node.extends('variable');
  */
 variable.prototype.consume = function(ast) {
   if (ast.kind === 'assign') {
-    this.name = ast.left.name;
-  } else {
+    if (ast.left.kind === 'variable') {
+      this.name = ast.left.name;
+    } else {
+      // @fixme should avoid this
+      this.name = '#' + ast.left.kind;
+    }
+    // resolve the variable type
+    var what = ast.right.kind;
+    if (what === 'number') {
+      this.type = 'number';
+    } else if (what === 'boolean') {
+      this.type = 'boolean';
+    } else if (what === 'string') {
+      this.type = 'string';
+    } else if (what === 'array') {
+      this.type = 'array';
+    } else if (what === 'new') {
+      this.type = reference.toClass(this, ast.right.what, 'new');
+    } else {
+      this.type = null;
+    }
+  } else if (this.kind === 'parameter') {
+    this.name = ast.name;
+    if (ast.type) {
+      this.type = reference.toClass(this, ast.type, 'type');
+    } else if (ast.value) {
+      // resolve from default value
+      var what = ast.value;
+      if (what === 'number') {
+        this.type = 'number';
+      } else if (what === 'boolean') {
+        this.type = 'boolean';
+      } else if (what === 'string') {
+        this.type = 'string';
+      } else if (what === 'array') {
+        this.type = 'array';
+      } else {
+        this.type = null;
+      }
+    }
+  } else if (this.kind === 'variable') {
     this.name = ast.name;
   }
 };
