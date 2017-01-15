10c3fcd59b5d8bc53898894de0ced190adbc6b16
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-15 12:28:51 +0100

    impl function

--- src/function.js
+++ src/function.js
@@ -8,13 +8,13 @@ var block = require('./block');
 
 /**
  * **Extends from [block](BLOCK.md)**
- * 
+ *
  * The namespace is an organisational unit that stores
  * inner nodes and use the namespace prefix to avoid
  * names colision.
- * 
+ *
  * @constructor function
- * 
+ *
  * @property {String} name The function name
  * @property {String} fullName
  * @property {Boolean} isClosure
@@ -22,5 +22,36 @@ var block = require('./block');
  * @property {variable[]} uses List of used variables
  */
 var fn = block.extends('function');
+
+/**
+ * @protected Consumes the current ast node
+ */
+fn.prototype.consume = function(ast) {
+  this.isClosure = ast.kind === 'closure';
+  this.uses = [];
+  if (this.isClosure) {
+    this.fullName = this.name = '#closure';
+    for(var i = 0; i < ast.uses.length; i++) {
+      // @fixme : should refer to an existing variable (if any)
+      this.uses.push(
+        ptr.create('variable', this, ast.uses[i])
+      );
+    }
+  } else {
+    this.name = ast.name;
+    this.fullName = this.getNamespace().name + '\\' + this.name;
+  }
+  // define arguments
+  this.args = [];
+  for(var i = 0; i < ast.arguments.length; i++) {
+    this.args.push(
+      ptr.create('variable', this, ast.arguments[i])
+    );
+  }
+  // parse inner body
+  if (ast.body && ast.body.children) {
+    this.scanForChilds(ast.body.children);
+  }
+};
 
 module.exports = fn;
