dd804662e3026a7f8e36a98e1f9cae3b6b9e4768
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-19 21:34:14 +0100

    implement file getter on includes/requires

--- src/external.js
+++ src/external.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (C) 2016 Glayzzle (BSD3 License)
+ * Copyright (C) 2017 Glayzzle (BSD3 License)
  * @authors https://github.com/glayzzle/php-reflection/graphs/contributors
  * @url http://glayzzle.com
  */
@@ -14,18 +14,18 @@ var expr = require('./expr');
  * Represents an external link (require or include)
  *
  * @public
- * @constructor external
+ * @constructor External
  * @property {String|expr} target {@link EXPR.md|:link:} The target file to be included
  * @property {Boolean} once If true it's a include_once or require_once statement
  * @property {Boolean} strict If true then it's a require statement
  */
-var _ext = node.extends('external');
+var External = node.extends('external');
 
 
 /**
  * @protected Consumes the current ast node
  */
-_ext.prototype.consume = function(ast) {
+External.prototype.consume = function(ast) {
   this.strict = ast.require;
   this.once = ast.once;
   this.target = expr.resolve(this, ast.target);
@@ -33,9 +33,23 @@ _ext.prototype.consume = function(ast) {
 
 /**
  * Try to resolve the target statement and retrive the file
- * @todo
- * @return {file} {@link FILE.md|:link:}
+ * @return {File|Promise} {@link FILE.md|:link:}
  */
-_ext.prototype.getTargetFile = function() {};
+External.prototype.getTargetFile = function() {
+  if (typeof this.target === 'string') {
+    if (!this._file) {
+      this._file = this.getRepository().get(this.target);
+      if (!this._file) {
+        return this.getRepository().parse(
+          this.target
+        ).then(function(file) {
+          this._file = file;
+        }.bind(this));
+      }
+    }
+    return this._file;
+  }
+  return null;
+};
 
-module.exports = _ext;
+module.exports = External;
