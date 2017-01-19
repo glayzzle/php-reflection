73e09e5f7f1068b28b0244f399556c3016b8b247
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-19 21:55:26 +0100

    https://github.com/glayzzle/php-reflection/issues/10 migrate method

--- src/method.js
+++ src/method.js
@@ -31,9 +31,26 @@ var Method = block.extends('method');
  * @protected Consumes the current ast node
  */
 Method.prototype.consume = function(ast) {
-  this.name = ast[1];
+  this.name = ast.name;
   this.fullName = this.parent.fullName + '::' + this.name;
-// @todo
+  // flags
+  this.isStatic = ast.isStatic;
+  this.isFinal = ast.isFinal;
+  this.isAbstract = ast.isAbstract;
+  this.isPublic = ast.visibility === 'public';
+  this.isProtected = ast.visibility === 'protected';
+  this.isPrivate = ast.visibility === 'private';
+  // define arguments
+  this.args = [];
+  for(var i = 0; i < ast.arguments.length; i++) {
+    this.args.push(
+      ptr.create('variable', this, ast.arguments[i])
+    );
+  }
+  // parse inner body
+  if (ast.body && ast.body.children) {
+    this.scanForChilds(ast.body.children);
+  }
 };
 
 module.exports = Method;
