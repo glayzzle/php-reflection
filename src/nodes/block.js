71ee42dd2874d662aca214696c986b2dd552da6f
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-02-18 23:39:16 +0100

    migrate blocks with new api

--- src/nodes/block.js
+++ src/nodes/block.js
@@ -5,7 +5,7 @@
  */
 'use strict';
 
-var node = require('../data/node');
+var Node = require('../data/node');
 var comment = require('../utils/comment');
 
 /**
@@ -14,7 +14,7 @@ var comment = require('../utils/comment');
  * Initialize a new file with the specified AST contents
  *
  * @public
- * @constructor block
+ * @constructor Block
  * @property {variable[]} variables {@link VARIABLE.md|:link:} A list of variables in current scope
  * @property {define[]} defines {@link DEFINE.md|:link:} A list of defined constants
  * @property {function[]} functions {@link FUNCTION.md|:link:} List of declared functions
@@ -23,35 +23,42 @@ var comment = require('../utils/comment');
  * @property {trait[]} traits {@link TRAIT.md|:link:} List of defined traits
  * @property {block[]} blocks {@link BLOCK.md|:link:} List of variable scoped blocks
  */
-var block = node.extends('block');
-
-/**
- * @protected Gets the current block
- */
-block.prototype.getBlock = function() {
-  return this;
-};
+var Block = Node.extends('block');
 
 /**
  * @protected Consumes the current ast node
  */
-block.prototype.consume = function(file, parent, ast) {
-    // super constructor
-    node.prototype.consume.apply(this, [file, parent, ast]);
-    this.scanForChilds(ast);
+Block.prototype.consume = function(file, parent, ast) {
+    this.consumeAST(ast);
+    Node.prototype.consume.apply(this, [file, parent, ast]);
 };
 
 /**
  * @protected Scan for inner childs
  */
-block.prototype.scanForChilds = function(ast) {
-  if (Array.isArray(ast)) {
-    for (var i = 0; i < ast.length; i++) {
-      this.consumeChild(ast[i]);
+Block.prototype.consumeAST = function(ast) {
+    // generic recusive scanner
+    for(var k in ast) {
+        var item = ast[k];
+        if (Array.isArray(item)) {
+            this.scanForChilds(item);
+        } else if (item && item.kind) {
+            this.consumeChild(item);
+        }
     }
-  } else if (ast && ast.kind) {
-    this.consumeChild(ast);
-  }
+};
+
+/**
+ * @protected Scan for inner childs
+ */
+Block.prototype.scanForChilds = function(ast) {
+    if (Array.isArray(ast)) {
+        for (var i = 0; i < ast.length; i++) {
+            this.consumeChild(ast[i]);
+        }
+    } else if (ast && ast.kind) {
+        this.consumeChild(ast);
+    }
 };
 
 /**
@@ -60,181 +67,155 @@ block.prototype.scanForChilds = function(ast) {
  * @param {Array} ast The AST node to eat
  * @return void
  */
-block.prototype.consumeChild = function(ast) {
-  if (!ast || !ast.kind) return;
+Block.prototype.consumeChild = function(ast) {
+    if (!ast || !ast.kind) return;
 
-  // handle class definition
-  if (ast.kind === 'doc') {
-    if (this.getRepository().options.scanDocs) {
-      this._lastDoc = item;
+    // handle class definition
+    if (ast.kind === 'doc') {
+        this._lastDoc = item;
+
     } else {
-      this._lastDoc = null;
-    }
-  } else {
 
-    // attach last doc node to current node
-    if (this._lastDoc) {
-      ast.doc = this._lastDoc;
-      this._lastDoc = null;
-    }
-
-    // scan a class
-    if (ast.kind === 'class') {
-      var cls = ptr.create('class', this, ast);
-      this.classes.push(cls);
-      if (this.type !== 'namespace') {
-        this.getNamespace().classes.push(cls);
-      }
-    }
-
-    // consome interface
-    else if (ast.kind === 'interface') {
-      var int = ptr.create('interface', this, ast);
-      this.interfaces.push(int);
-      if (this.type !== 'namespace') {
-        this.getNamespace().interfaces.push(cls);
-      }
-    }
-
-    // consome trait
-    else if (ast.kind === 'trait') {
-      var trait = ptr.create('trait', this, ast);
-      this.traits.push(trait);
-      if (this.type !== 'namespace') {
-        this.getNamespace().traits.push(trait);
-      }
-    }
-
-    // consome a namespace (from inner statements like declare)
-    else if (ast.kind === 'namespace') {
-      node.create('namespace', this.getFile(), ast);
-    }
-
-    // consome include statements
-    else if (ast.kind === 'include') {
-      ptr.create('external', this, ast);
-    }
-
-    // consume IF nodes
-    else if (ast.kind === 'if') {
-      if (this.getRepository().options.scanExpr) {
-
-        // IF BODY
-        if (ast.body) {
-          this.blocks.push(
-            ptr.create('block', this, ast.body)
-          );
+        // attach last doc node to current node
+        if (this._lastDoc) {
+            ast.doc = this._lastDoc;
+            this._lastDoc = null;
         }
-        // ELSE STATEMENT
-        if (ast.alternate) {
-          this.blocks.push(
-            ptr.create('block', this, ast.alternate)
-          );
+
+        // scan a class
+        if (ast.kind === 'class') {
+            var cls = this.graph.create('class', this, ast);
+            this.add('classes', cls);
+            if (this.type !== 'namespace') {
+                this.getNamespace().add('classes', cls);
+            }
         }
-      } else {
-        // inner scan only (ignore blocks)
-        if (ast.body) this.scanForChilds(ast.body);
-        if (ast.alternate) this.scanForChilds(ast.alternate);
-      }
-    }
 
-    // try nodes
-    else if (ast.kind === 'try') {
+        // consome interface
+        else if (ast.kind === 'interface') {
+            var int = this.graph.create('interface', this, ast);
+            this.add('interfaces', int);
+            if (this.type !== 'namespace') {
+                this.getNamespace().add('interfaces', cls);
+            }
+        }
 
-      if (this.getRepository().options.scanExpr) {
-        // BODY
-        this.blocks.push(
-          ptr.create('block', this, ast.body)
-        );
-        // CATCH
-        if (Array.isArray(ast.catches)) {
-          ast.catches.forEach(function(item) {
-            this.blocks.push(
-              ptr.create('block', this, item.body)
+        // consome trait
+        else if (ast.kind === 'trait') {
+            var trait = this.graph.create('trait', this, ast);
+            this.add('traits', trait);
+            if (this.type !== 'namespace') {
+                this.getNamespace().add('traits', trait);
+            }
+        }
+
+        // consome a namespace (from inner statements like declare)
+        else if (ast.kind === 'namespace') {
+            this.graph.create('namespace', this.getFile(), ast);
+        }
+
+        // consome include statements
+        else if (ast.kind === 'include') {
+            this.graph.create('external', this, ast);
+        }
+
+        // consume IF nodes
+        else if (ast.kind === 'if') {
+            if (this.getRepository().options.scanExpr) {
+                // IF BODY
+                if (ast.body) {
+                    this.graph.create('block', this, ast.body);
+                }
+                // ELSE STATEMENT
+                if (ast.alternate) {
+                    this.graph.create('block', this, ast.body);
+                }
+            } else {
+                // inner scan only (ignore blocks)
+                if (ast.body) this.scanForChilds(ast.body);
+                if (ast.alternate) this.scanForChilds(ast.alternate);
+            }
+        }
+
+        // try nodes
+        else if (ast.kind === 'try') {
+
+            if (this.getRepository().options.scanExpr) {
+                // BODY
+                this.graph.create('block', this, ast.body);
+                // CATCH
+                if (Array.isArray(ast.catches)) {
+                    ast.catches.forEach(function(item) {
+                        this.graph.create('block', this, item.body);
+                    }.bind(this));
+                }
+                // FINALLY
+                if (ast.allways) {
+                    this.graph.create('block', this, ast.allways);
+                }
+            } else {
+                // inner scan only (ignore blocks)
+                this.scanForChilds(ast.body);
+                if (Array.isArray(ast.catches)) this.scanForChilds(ast.catches);
+                if (ast.allways) this.scanForChilds(ast.allways);
+            }
+        }
+
+        // functions
+        else if (ast.kind === 'function') {
+            var fn = this.graph.create('function', this, ast);
+            this.add('functions', fn);
+            if (this.type !== 'namespace') {
+                this.getNamespace().add('functions', fn);
+            }
+        }
+
+        // variables (by assignment)
+        else if (
+            ast.kind === 'assign' &&
+            ast.left.kind === 'variable' &&
+            this.getRepository().options.scanVars
+        ) {
+            this.add(
+                'variables',
+                this.graph.create('variable', this, ast)
             );
-          }.bind(this));
         }
-        // FINALLY
-        if (ast.allways) {
-          this.blocks.push(
-            ptr.create('block', this, ast.allways)
-          );
+
+        // extract defines
+        else if (ast.kind === 'call' && ast.what.name === 'define') {
+            this.add(
+                'defines',
+                this.graph.create('define', this, ast)
+            );
         }
-      } else {
-        // inner scan only (ignore blocks)
-        this.scanForChilds(ast.body);
-        if (Array.isArray(ast.catches)) this.scanForChilds(ast.catches);
-        if (ast.allways) this.scanForChilds(ast.allways);
-      }
-    }
 
-    // functions
-    else if (ast.kind === 'function') {
-      var fn = ptr.create('function', this, ast);
-      this.functions.push(fn);
-      if (this.type !== 'namespace') {
-        this.getNamespace().functions.push(fn);
-      }
-    }
-
-    // variables (by assignment)
-    else if (
-      ast.kind === 'assign' &&
-      ast.left.kind === 'variable' &&
-      this.getRepository().options.scanVars
-    ) {
-      this.variables.push(
-        ptr.create('variable', this, ast)
-      );
-    }
-
-    // extract defines
-    else if (ast.kind === 'call' && ast.what.name === 'define') {
-      this.defines.push(
-        ptr.create('define', this, ast)
-      );
-    }
-
-    // namespace constants
-    else if (ast.kind === 'constant') {
-      this.getNamespace().constants.push(
-        ptr.create('constant', this, ast)
-      );
-    }
-
-    // namespace use statements
-    else if (ast.kind === 'usegroup') {
-      var prefix = ast.name ? ast.name.name : '';
-      for(var i = 0; i < ast.items.length; i++) {
-        var alias = ast.items[i].alias;
-        var name = ast.items[i].name.name;
-        if (name[0] !== '\\') name = '\\' + name;
-        if (!alias) {
-          alias = name.split('\\');
-          alias = alias[alias.length - 1];
+        // namespace constants
+        else if (ast.kind === 'constant') {
+            this.getNamespace().add(
+                'constants',
+                this.graph.create('constant', this, ast)
+            );
         }
-        this.getNamespace().uses[alias] = prefix + name;
-      }
-    }
 
-    // @todo : variables by global statement
-    // @todo : variables by static statement
-
-    // nested childs
-    else {
-      // generic recusive scanner
-      for(var k in ast) {
-        var item = ast[k];
-        if (Array.isArray(item)) {
-          this.scanForChilds(item);
-        } else if (item && item.kind) {
-          this.consumeChild(item);
+        // namespace use statements
+        else if (ast.kind === 'usegroup') {
+            this.getNamespace().add(
+                'uses',
+                this.graph.create('usegroup', this, ast)
+            );
         }
-      }
+
+        // @todo : variables by global statement
+        // @todo : variables by static statement
+
+        // nested childs
+        else {
+            this.consumeAST(ast);
+        }
     }
-  }
 
   /* @todo var */
 };
 
-module.exports = block;
+module.exports = Block;
